65d315e7d5f6167ed1aedc55aeaa1b46f84967ff
KpwnZ <22996989+KpwnZ@users.noreply.github.com>
2022-08-10 23:24:35 +0800

    insert decode routine to each function.

--- StringObf.cpp
+++ StringObf.cpp
@@ -3,8 +3,12 @@
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Instruction.h"
 
 #include <random>
+#include <vector>
+#include <set>
+#include <map>
 
 namespace {
 
@@ -12,31 +16,181 @@ class GlobalStringVariable {
 private:
     std::vector<uint8_t> enc;
     std::vector<uint8_t> key;
+    std::vector<uint8_t> str;
     llvm::GlobalVariable *glob;
+    llvm::Instruction *referInstruction;
 public:
     GlobalStringVariable(std::vector<uint8_t> &v1, std::vector<uint8_t> &v2, llvm::GlobalVariable *g) 
         : enc(v1), key(v2), glob(g) { }
+    GlobalStringVariable(std::vector<uint8_t> &v1, std::vector<uint8_t> &v2, llvm::GlobalVariable *g, llvm::Instruction *i) 
+        : enc(v1), key(v2), glob(g), referInstruction(i) { }
+    GlobalStringVariable(std::vector<uint8_t> &v1, std::vector<uint8_t> &v2, llvm::GlobalVariable *g, llvm::Instruction *i, std::vector<uint8_t> &buf) 
+        : enc(v1), key(v2), glob(g), referInstruction(i), str(buf) { }
     llvm::GlobalVariable *getGlob() { return glob; }
     std::vector<uint8_t> &getEnc()  { return enc;  }
     std::vector<uint8_t> &getKey()  { return key;  }
+    llvm::Instruction *getReferInstruction() { return referInstruction; }
 };
+
+bool isStringVariable(llvm::GlobalVariable *glob) {
+    if (!glob->hasInitializer() || glob->hasExternalLinkage()) return false;
+    llvm::StringRef section = glob->getSection();
+    if (section == "llvm.metadata") return false;
+
+    llvm::Constant *initializer = glob->getInitializer();
+    if (!initializer) return false;
+
+    if (isa<llvm::ConstantDataArray>(initializer)) {
+        auto arr = cast<llvm::ConstantDataArray>(initializer);
+        // llvm::errs() << arr->getAsString().begin() << "\n";
+        if (arr->isString()) return true;
+    }
+    return false;
+}
+
+bool isStringLinkage(llvm::GlobalVariable *glob) {
+    if (!glob->hasExternalLinkage()) return false;
+
+    llvm::Constant *initializer = glob->getInitializer();
+    if (!initializer) return false;
+
+    for (auto &op : initializer->operands()) {
+        auto *g = dyn_cast<llvm::GlobalVariable>(op->stripPointerCasts());
+        if(g && isStringVariable(g)) {
+            return true;
+        }
+    }
+    return false;
+}
 
 struct StringObf : llvm::PassInfoMixin<StringObf> {
 
     llvm::PreservedAnalyses run(llvm::Module &M, llvm::ModuleAnalysisManager &MAM) {
-        
-        std::vector<GlobalStringVariable> globalStrings = getStrings(M);
-
-        // create decode functions
-        auto *f = buildDecodeFunction(M);
-
-        // create decode caller
-        auto *caller = buildDecodeCallerFunction(M, globalStrings, f);
-
-        // inject call
-        injectDecodeCall(M, caller);
-
+        std::set<llvm::GlobalVariable *> processed;
+        std::map<llvm::GlobalVariable *, llvm::GlobalVariable *> mapEnc2Key;
+        for(auto &F : M) {
+            processFunction(F, M, processed, mapEnc2Key);
+        }
         return llvm::PreservedAnalyses::all();
+    }
+
+    void processFunction(llvm::Function &F, llvm::Module &M, 
+                         std::set<llvm::GlobalVariable *> &processed, 
+                         std::map<llvm::GlobalVariable *, llvm::GlobalVariable *> &mapEnc2Key) {
+
+        std::vector<GlobalStringVariable> globalStrings;
+        std::uniform_int_distribution<uint8_t> distribution(0, 255);
+        std::default_random_engine generator;
+
+        for(auto &BB : F) {
+            for (auto &inst : BB) {
+                for(auto &op : inst.operands()) {
+                    auto *glob = dyn_cast<llvm::GlobalVariable>(op->stripPointerCasts());
+                    const char *buf = nullptr;
+                    size_t len = 0;
+                    if(glob) {
+                        if(isStringVariable(glob)) {
+                            llvm::Constant *initializer = glob->getInitializer();
+                            auto arr = cast<llvm::ConstantDataArray>(initializer);
+
+                            buf = arr->getAsString().begin();
+                            len = arr->getAsString().size();
+                        } else if(isStringLinkage(glob)) {
+                            // check operands
+                            llvm::Constant *initializer = glob->getInitializer();
+                            for(auto &op : initializer->operands()) {
+                                auto *g = dyn_cast<llvm::GlobalVariable>(op->stripPointerCasts());
+                                if(g) {
+                                    if(isStringVariable(g)) {
+                                        auto arr = cast<llvm::ConstantDataArray>(g->getInitializer());
+                                        buf = arr->getAsString().begin();
+                                        len = arr->getAsString().size();
+                                        glob = g;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        if(!buf || !len) continue;
+
+                        // generate xor key and encode the string
+                        std::vector<uint8_t> enc_str, xor_key, ori;
+                        for(size_t i = 0; i < len; ++i) {
+                            uint8_t key = distribution(generator);
+                            ori.push_back(buf[i]);
+                            xor_key.push_back(key);
+                            enc_str.push_back(buf[i] ^ key);
+                        }
+
+                        globalStrings.push_back(
+                            {
+                                enc_str, xor_key, glob, &inst, ori
+                            }
+                        );
+                        llvm::Constant *enc_dt = llvm::ConstantDataArray::get(glob->getParent()->getContext(),
+                                                                      llvm::ArrayRef<uint8_t>(enc_str));
+                        // glob->setInitializer();
+                        glob->setConstant(false);
+                    }
+                }
+            }
+        }
+        // inject decode instruction for current function F
+        injectDecodeInst(F, M, globalStrings, processed, mapEnc2Key);
+    }
+
+    void injectDecodeInst(llvm::Function &F, llvm::Module &M, 
+                          std::vector<GlobalStringVariable> &globalStrings, 
+                          std::set<llvm::GlobalVariable *> &processed, 
+                          std::map<llvm::GlobalVariable *, llvm::GlobalVariable *> &mapEnc2Key) {
+        // inject decode instruction
+        auto &ctx = F.getContext();
+        for(auto &g : globalStrings) {
+            llvm::IRBuilder<> builder(g.getReferInstruction());
+
+            auto *glob = g.getGlob();
+            llvm::GlobalVariable *key_glob = nullptr;
+
+            if(processed.find(glob) == processed.end()) {
+                llvm::Constant *enc_dt = llvm::ConstantDataArray::get(glob->getParent()->getContext(),
+                                                                      llvm::ArrayRef<uint8_t>(g.getEnc()));
+                glob->setInitializer(enc_dt);
+                processed.insert(glob);
+                key_glob = new llvm::GlobalVariable(
+                    M,
+                    glob->getType()->getElementType(),
+                    false, 
+                    glob->getLinkage(),
+                    nullptr, 
+                    glob->getName(),
+                    nullptr,
+                    glob->getThreadLocalMode(),
+                    glob->getType()->getAddressSpace()
+                );
+                mapEnc2Key[glob] = key_glob;
+                key_glob->setInitializer(llvm::ConstantDataArray::get(glob->getParent()->getContext(),
+                                                                  llvm::ArrayRef<uint8_t>(g.getKey())));
+
+            } else {
+                key_glob = mapEnc2Key[glob];
+            }
+            auto *enc_str = builder.CreatePointerCast(glob, llvm::Type::getInt8PtrTy(ctx));
+            auto *key_str = builder.CreatePointerCast(key_glob, llvm::Type::getInt8PtrTy(ctx));
+
+            for (size_t i = 0; i < g.getEnc().size(); ++i) {
+                auto *cur_p = builder.CreateGEP(enc_str, llvm::ConstantInt::getSigned(llvm::IntegerType::get(ctx, 64), i));
+                auto *cur_key_p = builder.CreateGEP(key_str, llvm::ConstantInt::getSigned(llvm::IntegerType::get(ctx, 64), i));
+                auto *cur = builder.CreateLoad(llvm::Type::getInt8Ty(ctx), cur_p, false);
+                auto *cur_key = builder.CreateLoad(llvm::Type::getInt8Ty(ctx), cur_key_p, false);
+                auto *cur32 = builder.CreateSExt(cur, llvm::Type::getInt32Ty(ctx));
+                auto *cur_key32 = builder.CreateSExt(cur_key, llvm::Type::getInt32Ty(ctx));
+                auto *xored = builder.CreateXor(cur32, cur_key32);
+                auto *xored8 = builder.CreateTrunc(xored, llvm::Type::getInt8Ty(ctx));
+                auto *store1 = builder.CreateStore(xored8, cur_p);
+                auto *store2 = builder.CreateStore(llvm::ConstantInt::getSigned(llvm::IntegerType::get(ctx, 8), 0), cur_key_p);
+            }
+
+        }
     }
 
     void injectDecodeCall(llvm::Module &M, llvm::Function *f) {
@@ -49,6 +203,7 @@ struct StringObf : llvm::PassInfoMixin<StringObf> {
         llvm::IRBuilder<> *builder = new llvm::IRBuilder<>(bb);
         builder->CreateCall(f);
         builder->CreateBr(&b);
+        if(builder) delete builder;
     }
 
     llvm::Function *buildDecodeCallerFunction(llvm::Module &M, std::vector<GlobalStringVariable> &globalStrings, llvm::Function *decodeFunction) {
@@ -72,7 +227,7 @@ struct StringObf : llvm::PassInfoMixin<StringObf> {
             auto key_glob = new llvm::GlobalVariable(
                 M,
                 glob->getType()->getElementType(),
-                true, 
+                false, 
                 glob->getLinkage(),
                 nullptr, 
                 glob->getName(),
